cfb03311a4c7c052d18ae61d0c07f67ce43d2a95
vladimir <vladimir197717@mail.ru>
2023-05-27 18:32:17 +0400

    add Lombok in MyEventListener

--- src/main/java/ru/learnUp/homework/Task20_JAVA2_EventAndLocation_SpringBoot/MyEventListener.java
+++ src/main/java/ru/learnUp/homework/Task20_JAVA2_EventAndLocation_SpringBoot/MyEventListener.java
@@ -1,5 +1,6 @@
 package ru.learnUp.homework.Task20_JAVA2_EventAndLocation_SpringBoot;
 
+import lombok.Data;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -10,13 +11,10 @@ import static ru.learnUp.homework.Task20_JAVA2_EventAndLocation_SpringBoot.Guess
 import static ru.learnUp.homework.Task20_JAVA2_EventAndLocation_SpringBoot.GuessingGame.randomNumber;
 
 @Service
+@Data
 public class MyEventListener implements ApplicationListener<MyEvent>, ApplicationContextAware {
     private ApplicationContext context;
     private String message = " ";
-
-    public String getMessage() {
-        return message;
-    }
 
     @Override
     public void onApplicationEvent(MyEvent event) {
